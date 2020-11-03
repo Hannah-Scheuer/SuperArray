2f14e148e44b345abf493694b5e634b3d9ff090e
Hannah-Scheuer <hscheuer20@gmail.com>
2020-11-02 19:45:30 -0500

    fixed some small errors

--- SuperArray.java
+++ SuperArray.java
@@ -8,8 +8,8 @@ public class SuperArray{
   }
 
   public int size(){
-    size = data.length();
-    for (int i = 0;i<data.length();i++){
+    size = data.length;
+    for (int i = 0;i<data.length;i++){
       if (data[i]==null){
         size = i;
         return i;
@@ -19,17 +19,17 @@ public class SuperArray{
   }
 
   public void resize(){
-    int hello[] = new String[data.length()+1];
-    for (int i=0; i < data.length(); i++){
+    String hello[] = new String[data.length+1];
+    for (int i=0; i < data.length; i++){
       hello[i] = data[i];
     }
       data = hello;
   }
 
   public boolean add(String element){
-    if (size>data.length()){
+    if (size>data.length){
       data.resize();
-      data[data.length()-1]=element;
+      data[data.length-1]=element;
     }
     else{
       data[data.size()]=element;
